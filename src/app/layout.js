7a094517d361c4abf173791fcc65d3dd2a3431ca
Elie Parthenay <elie.parthenay@gmail.com>
2024-06-05 10:54:44 +0200

    Init project

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Destination Numérique - Template Next.js",
+  description: "Destination Numérique - Template Next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <main>
+      <h1>Destination Numérique - Template Next.js</h1>
+    </main>
+  );
+}
